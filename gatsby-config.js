b580f14ce98cc5fa99c9402250ea9fce6e141fa9
Finley <finleyjchen@gmail.com>
2021-02-10 13:51:31 -0800

    added gatsby plugin offline and updated manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -53,13 +53,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Finley Chen`,
+        short_name: `Finley Chen`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#2b2b2b`,
+        theme_color: `#ffffff`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/fc-logo-favicon.png`, // This path is relative to the root of the site.
       },
     },
     {
@@ -72,6 +72,6 @@ module.exports = {
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    `gatsby-plugin-offline`,
   ],
 }
